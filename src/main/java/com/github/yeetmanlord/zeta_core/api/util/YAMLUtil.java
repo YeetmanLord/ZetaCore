6412bda8a08e55d9d6fd64954a422db2747d8ded
YeetmanLord <60905636+YeetmanLord@users.noreply.github.com>
2023-01-25 15:15:51 +0000

    Adding changes that were accidently pushed to main. (#3)
    * Merging from dev into main. (#1)
    
    * Added some prebuilt animations.
    
    * Fixing some database stuff.
    
    * Updated version strings
    
    * Update YAMLUtil.java
    
    * Dev (#2)
    
    * Added some prebuilt animations.
    
    * Fixing some database stuff.

--- src/main/java/com/github/yeetmanlord/zeta_core/api/util/YAMLUtil.java
+++ src/main/java/com/github/yeetmanlord/zeta_core/api/util/YAMLUtil.java
@@ -7,136 +7,115 @@ import javax.annotation.Nullable;
 
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.json.simple.JSONObject;
 
 import com.github.yeetmanlord.zeta_core.data.DataStorer;
 
 /**
  * @author YeetManLord
- *
  * @zeta.usage INTERNAL This is only for storing and reading data from database
  */
 @SuppressWarnings("unchecked")
 public class YAMLUtil {
 
-	/**
-	 * @param config The {@link FileConfiguration} to read from
-	 * @param path The config path to read from
-	 * @return A JSON object containing the data at the specified path
-	 */
-	public static JSONObject getJSONFromYAML(FileConfiguration config, @Nullable String path) {
+    /**
+     * Recursively reads a {@link ConfigurationSection} or entire into a map
+     * @param config The {@link FileConfiguration} to read from
+     * @param path   The config path to read from
+     * @return A map containing the data at the specified path
+     */
+    public static Map<String, Object> getMapFromYAML(FileConfiguration config, @Nullable String path) {
 
-		Map<String, Object> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
 
-		if (path != null) {
+        if (path != null) {
 
-			for (String key : config.getConfigurationSection(path).getKeys(false)) {
-				Object obj = config.get(path + "." + key);
+            for (String key : config.getConfigurationSection(path).getKeys(false)) {
+                Object obj = config.get(path + "." + key);
 
-				if (obj instanceof ConfigurationSection) {
-					recurse(map, (ConfigurationSection) obj, key);
-				}
-				else {
-					map.put(key, config.get(path + "." + key));
-				}
+                if (obj instanceof ConfigurationSection) {
+                    recurse(map, (ConfigurationSection) obj, key);
+                } else {
+                    map.put(key, config.get(path + "." + key));
+                }
 
-			}
+            }
 
-			return new JSONObject(map);
-		}
+            return map;
+        }
 
-		for (String key : config.getKeys(false)) {
+        for (String key : config.getKeys(false)) {
 
-			Object obj = config.get(key);
+            Object obj = config.get(key);
 
-			if (obj instanceof ConfigurationSection) {
-				recurse(map, (ConfigurationSection) obj, key);
-			}
-			else {
-				map.put(key, config.get(key));
-			}
+            if (obj instanceof ConfigurationSection) {
+                recurse(map, (ConfigurationSection) obj, key);
+            } else {
+                map.put(key, config.get(key));
+            }
 
-		}
+        }
 
-		return new JSONObject(map);
+        return map;
 
-	}
+    }
 
-	/**
-	 * @param dataStorer The config to write to
-	 * @param path The section to write to
-	 * @param json The JSON data to write
-	 */
-	public static void writeYAMLFromJSON(DataStorer dataStorer, @Nullable String path, JSONObject json) {
+    /**
+     * Quickly write a map to a config
+     * @param dataStorer The config to write to
+     * @param path       The section to write to
+     * @param json       The map data to write
+     * @param shouldSave Whether to save the config after writing
+     */
+    public static void writeYAMLFromMap(DataStorer dataStorer, @Nullable String path, Map<String, Object> json, boolean shouldSave) {
 
-		FileConfiguration config = dataStorer.get();
+        FileConfiguration config = dataStorer.get();
 
-		if (path != null) {
+        if (path != null) {
 
-			config.createSection(path, json);
+            config.createSection(path, json);
 
-		}
-		else {
+        } else {
 
-			for (Object key : json.keySet()) {
+            for (String key : json.keySet()) {
 
-				if (key instanceof String) {
-					Object obj = json.get(key);
+                Object obj = json.get(key);
 
-					if (obj instanceof Map) {
-						recurseWrite(json, config.createSection((String) key));
-					}
-					else {
-						config.set((String) key, json.get(key));
-					}
+                if (obj instanceof Map) {
+                    config.createSection(key, (Map<?, ?>) obj);
+                } else {
+                    config.set(key, json.get(key));
+                }
 
-				}
-				else {
-					throw (new IllegalArgumentException("The given JSONObject contains non-string keys making it incompatible with YAML"));
-				}
+            }
 
-			}
+        }
 
-		}
+        if (shouldSave)
+            dataStorer.save();
 
-		dataStorer.save();
+    }
 
-	}
+    public static void writeYAMLFromMap(DataStorer dataStorer, @Nullable String path, Map<String, Object> json) {
+        writeYAMLFromMap(dataStorer, path, json, true);
+    }
 
-	private static void recurse(Map<String, Object> map, ConfigurationSection section, String key1) {
+    private static void recurse(Map<String, Object> map, ConfigurationSection section, String key1) {
 
-		Map<String, Object> newMap = new HashMap<>();
+        Map<String, Object> newMap = new HashMap<>();
 
-		for (String key : section.getKeys(false)) {
-			Object obj = section.get(key);
+        for (String key : section.getKeys(false)) {
+            Object obj = section.get(key);
 
-			if (obj instanceof ConfigurationSection) {
-				recurse(newMap, (ConfigurationSection) obj, key);
-			}
-			else {
-				newMap.put(key, section.get(key));
-			}
+            if (obj instanceof ConfigurationSection) {
+                recurse(newMap, (ConfigurationSection) obj, key);
+            } else {
+                newMap.put(key, section.get(key));
+            }
 
-		}
+        }
 
-		map.put(key1, newMap);
+        map.put(key1, newMap);
 
-	}
-
-	private static void recurseWrite(Map<String, Object> map, ConfigurationSection section) {
-
-		for (String key : map.keySet()) {
-			Object obj = map.get(key);
-
-			if (obj instanceof Map) {
-				recurseWrite((Map<String, Object>) obj, section.createSection(key));
-			}
-			else {
-				section.set(key, obj);
-			}
-
-		}
-
-	}
+    }
 
 }
